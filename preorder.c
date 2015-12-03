99d340c9332007b3bf7f3547ac20f6a4e8b54caf
nareshpothula <nareshpothulas@gmail.com>
2015-12-03 14:32:08 +0530

    Added all tree programs

---
+++ preorder.c
@@ -0,0 +1,92 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct TREE
+{
+struct TREE *left;
+int num;
+struct TREE *right;
+};
+
+void add_TREE(struct TREE **ptr,int);
+void inorder(struct TREE *);
+void preorder(struct TREE *);
+void postorder(struct TREE *);
+main()
+{
+struct TREE *headptr=0;
+add_TREE(&headptr,20);
+add_TREE(&headptr,20);
+add_TREE(&headptr,20);
+add_TREE(&headptr,22);
+add_TREE(&headptr,17);
+add_TREE(&headptr,10);
+add_TREE(&headptr,20);
+add_TREE(&headptr,25);
+inorder(headptr);
+preorder(headptr);
+postorder(headptr);
+}
+void add_TREE (struct TREE **ptr,int n)
+{
+if(*ptr==0)
+{
+	(*ptr)=malloc(sizeof(struct TREE));
+	(*ptr)->num=n;
+	(*ptr)->left=(*ptr)->right=0;
+}
+else if((*ptr)->num < n)
+{
+	add_TREE (&((*ptr)->right),n);
+}
+else
+{
+	add_TREE(&((*ptr)->left),n);
+ }
+}
+
+void inorder(struct TREE *ptr)
+{
+if(ptr)
+{
+inorder(ptr->left);
+printf("%d\t",ptr->num);
+inorder(ptr->right);
+
+}
+
+}
+void preorder(struct TREE *ptr)
+{
+if(ptr)
+{
+printf("%d\t",ptr->num);
+preorder(ptr->left);
+preorder(ptr->right);
+}
+}
+void postorder(struct TREE *ptr)
+{
+if(ptr)
+{
+postorder(ptr->left);
+postorder(ptr->right);
+printf("%d\t",ptr->num);
+}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ s1.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+main()
+{
+
+printf("hello\n");
+while(1);
+
+}
+
